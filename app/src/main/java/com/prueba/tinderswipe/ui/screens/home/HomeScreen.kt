a8328e0c8e040075831c388b0a1346cef5b993c8
Christopher <130204738+BeboCod@users.noreply.github.com>
2025-02-08 18:22:05 -0600

    feat: Update application icon and name to Dragon Ball Z theme
    - Changed the application name to "Tinder Dragon Ball Z".
    - Replaced the application icon with a new icon set, including foreground and background layers, for adaptive icons.
    - Added new drawable resources for the application icon.
    - Removed old icon resources.
    - Added new `.name` file to the project to define the application's name.
    - Updated the `HomeScreen` code to remove the logo in the top bar.

--- app/src/main/java/com/prueba/tinderswipe/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/prueba/tinderswipe/ui/screens/home/HomeScreen.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.rememberAsyncImagePainter
 import com.prueba.tinderswipe.R
 import com.prueba.tinderswipe.network.model.DragonBallCharacter
@@ -130,17 +131,6 @@ fun TopBar(
             }
         },
         title = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.tinder_2),
-                    contentDescription = "Logo",
-                    modifier = Modifier.size(32.dp)
-                )
-            }
         }
     )
 }
